6f9418bf425cbf960c278c55ac233967b0eb0310
adeline.gueniat <gueniat.adeline@gmail.com>
2017-12-12 10:54:48 +0100

    Add constructor to Converter.java Initialize a window

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,3 +1,12 @@
-public class Converter {
+import javax.swing.JFrame;
+
+public class Converter extends JFrame {
+  public Converter(){
+    this.setTitle("Mon convertisseur");
+    this.setSize(400, 500);
+    this.setLocationRelativeTo(null);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+  }
 
 }
